30778a1dd3e842002b891e21fe9c5c45b3380c5d
saikat <saikatchakrabortty2@gmail.com>
2018-10-25 18:15:04 +0530

    Force it

--- src/routes/computex/Computex.js
+++ src/routes/computex/Computex.js
@@ -5,11 +5,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-
+import axios from 'axios';
 class Computex extends React.Component {
   static propTypes = {
     title: PropTypes.string.isRequired
   };
+  componentDidMount=()=>{
+  axios.get('/apis/ping', {
+          headers: {
+            authorization: JSON.parse(localStorage.getItem('token')),
+          },
+        })
+        .then(data => {
+          if(data.data){
+          console.log('Everything is fine bro')
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });
+  }
 
   render() {
     return (
