fd0b6d7f2765dd126f0e691ddfc3337e0ad0b3b7
saikat <saikatchakrabortty2@gmail.com>
2018-11-13 17:23:12 +0530

    Completeing Listing Creation for Buy and sell module

--- src/routes/add-p2p/Add-p2p.js
+++ src/routes/add-p2p/Add-p2p.js
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import axios from 'axios';
 import s from './Add-p2p.css';
-import { Card, Form, Input, Button, Select } from 'antd';
+import { Card, Form, Input, Button, Select, Radio } from 'antd';
+
+const FormItem = Form.Item;
+const Option = Select.Option;
+const RadioGroup = Radio.Group;
 
 const tabListNoTitle = [
   {
@@ -16,52 +20,86 @@ const tabListNoTitle = [
   },
 ];
 
-const FormItem = Form.Item;
-const Option = Select.Option;
-
 class BuyComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       showPaymentInputBox: false,
+      atPrice: '',
     };
   }
-    getCurrentBtcValue =async()=>{
-      let data;
-      try{
-        data = await axios.get('https://localbitcoins.com/equation/btc_in_usd*1');
-        if(data && !data.data){
+  componentDidMount = () => {
+    this.getCurrentBtcValue();
+  };
+  // componentDidUpdate=()=>{
+  //   if(this.state.currency){
+  //     this.getCurrentBtcValue(this.state.currency);
+  //   }
+  // }
+  getCurrentBtcValue = (CUR = 'INR') => {
+    axios
+      .get(`/apis/cur/current_BTC?currency=${CUR}`)
+      .then(data => {
+        if (data && data.data) {
+          this.setState({
+            currentBtc: data.data.data,
+          });
+        } else {
           //there is some status code error check and send notification accordingly
-        }else{
-          return data.data;
         }
-      }catch(error){
+      })
+      .catch(error => {
+        console.log(error);
         //throw a notifiation cannot connect or something
-      }
-    };
+      });
+  };
   handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
         console.log('Received values of form: ', JSON.stringify(values));
         this.setState(values);
+        if (this.props.sell) {
+          //create sell listing here
+          axios.post('/apis/p2p/add_sell_listing', values).then(data => {
+            if (data) {
+              console.log(data.data);
+              location.href = '/p2p';
+            } else {
+              console.log(data);
+            }
+          });
+        } else {
+          //create buy listing here
+          axios.post('/apis/p2p/add_buy_listing', values).then(data => {
+            if (data) {
+              console.log(data.data);
+              location.href = '/p2p';
+            } else {
+              console.log(data);
+            }
+          });
+        }
       }
     });
   };
-  handleSelectChange1st = (value) => {
-
+  onRadioChange = value => {
+    this.setState({
+      atPrice: value.target.value,
+    });
+  };
+  handleSelectChange1st = value => {
     if (!('value' in this.props)) {
-      this.setState({ paymentMethod:value });
+      this.setState({ paymentMethod: value });
     }
     const onChange = this.props.onChange;
     if (onChange) {
       onChange(Object.assign({}, this.state, value));
     }
   };
-  handleSelectChange2nd = (value) => {
-
+  handleSelectChange2nd = value => {
     if (!('value' in this.props)) {
-      this.setState({ currency:value });
+      this.setState({ currency: value });
     }
     const onChange = this.props.onChange;
     if (onChange) {
@@ -105,106 +143,168 @@ class BuyComponent extends React.Component {
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
-      <Form onSubmit={this.handleSubmit}>
-        <FormItem
-          label="Headline"
-          labelCol={{ span: 5 }}
-          wrapperCol={{ span: 12 }}
-        >
-          {getFieldDecorator('headLine', {
-            rules: [{ required: true, message: 'Headline is required!' }],
-          })(
-            <Input
-              placeholder=" e.g Sell bitcoins using PayTM with Indian Rupee (INR)"
-              onChange={this.formchange}
-              name="headLine"
-            />,
-          )}
-        </FormItem>
-        <FormItem
-          label="Payment Method"
-          labelCol={{ span: 5 }}
-          wrapperCol={{ span: 12 }}
-        >
-          {getFieldDecorator('paymentMethod', {
-            rules: [
-              { required: true, message: 'Please select Payment Method!' },
-            ],
-          })(
-            <Select
-              mode="combobox"
-              name="paymentMethod"
-              value={this.props.paymentMethod}
-              placeholder="Select a payment Method"
-              onChange={this.handleSelectChange1st}
-            >
-              {this.childrenPayList()}
-            </Select>,
-          )}
-        </FormItem>
-        <FormItem
-          label="currency"
-          labelCol={{ span: 5 }}
-          wrapperCol={{ span: 12 }}
-        >
-          {getFieldDecorator('currency', {
-            rules: [{ required: true, message: 'Please select currency!' }],
-          })(
-            <Select
-              mode="combobox"
-              name="currency"
-              value={this.props.currency}
-              placeholder="Select a currency"
-              onChange={this.handleSelectChange2nd}
-            >
-              {this.childrenCurrList()}
-            </Select>,
-          )}
-        </FormItem>
-        <FormItem label="Note" labelCol={{ span: 5 }} wrapperCol={{ span: 12 }}>
-          {getFieldDecorator('note', {
-            rules: [{ required: true, message: 'Please attach a message!' }],
-          })(
-            <Input
-              placeholder="Write a special Note"
-              onChange={this.formchange}
-              name="specialNote"
-            />,
-          )}
-        </FormItem>
-        <FormItem label="Maximum" labelCol={{ span: 5 }} wrapperCol={{ span: 12 }}>
-          {getFieldDecorator('maximum', {
-            rules: [{ required: true, message: 'Please enter mmaximum limit!' }],
-          })(
-            <Input
-              type="number"
-              placeholder="maximum amount"
-              onChange={this.formchange}
-              name="maxAmt"
-              addonAfter={this.state.currency || "USD"}
-            />,
-          )}
-        </FormItem>
-        <FormItem label="Minimum" labelCol={{ span: 5 }} wrapperCol={{ span: 12 }}>
-          {getFieldDecorator('minimum', {
-            rules: [{ required: true, message: 'Please enter minimum limit!' }],
-          })(
-            <Input
-              type="number"
-              placeholder="minimum amount"
-              onChange={this.formchange}
-              name="minAmt"
-              addonAfter={this.state.currency || "USD"}
-            />,
-          )}
-        </FormItem>
+      <div>
+        <label>Current Btc Price: {this.state.currentBtc} INR</label>
+        <Form onSubmit={this.handleSubmit}>
+          <FormItem
+            label="Headline"
+            labelCol={{ span: 5 }}
+            wrapperCol={{ span: 12 }}
+          >
+            {getFieldDecorator('headLine', {
+              rules: [{ required: true, message: 'Headline is required!' }],
+            })(
+              <Input
+                placeholder=" e.g Sell bitcoins using PayTM with Indian Rupee (INR)"
+                onChange={this.formchange}
+                name="headLine"
+              />,
+            )}
+          </FormItem>
+          <FormItem
+            label="Payment Method"
+            labelCol={{ span: 5 }}
+            wrapperCol={{ span: 12 }}
+          >
+            {getFieldDecorator('paymentMethod', {
+              rules: [
+                { required: true, message: 'Please select Payment Method!' },
+              ],
+            })(
+              <Select
+                mode="combobox"
+                name="paymentMethod"
+                value={this.props.paymentMethod}
+                placeholder="Select a payment Method"
+                onChange={this.handleSelectChange1st}
+              >
+                {this.childrenPayList()}
+              </Select>,
+            )}
+          </FormItem>
+          <FormItem
+            label="currency"
+            labelCol={{ span: 5 }}
+            wrapperCol={{ span: 12 }}
+          >
+            {getFieldDecorator('currency', {
+              rules: [{ required: true, message: 'Please select currency!' }],
+            })(
+              <Select
+                mode="combobox"
+                name="currency"
+                value={this.props.currency}
+                placeholder="Select a currency"
+                onChange={this.handleSelectChange2nd}
+              >
+                {this.childrenCurrList()}
+              </Select>,
+            )}
+          </FormItem>
+          <FormItem
+            label="Note"
+            labelCol={{ span: 5 }}
+            wrapperCol={{ span: 12 }}
+          >
+            {getFieldDecorator('note', {
+              rules: [{ required: true, message: 'Please attach a message!' }],
+            })(
+              <Input.TextArea
+                rows={5}
+                placeholder="Write a special Note"
+                onChange={this.formchange}
+                name="specialNote"
+              />,
+            )}
+          </FormItem>
+          <FormItem
+            label="Maximum"
+            labelCol={{ span: 5 }}
+            wrapperCol={{ span: 12 }}
+          >
+            {getFieldDecorator('maximum', {
+              rules: [
+                { required: true, message: 'Please enter mmaximum limit!' },
+              ],
+            })(
+              <Input
+                type="number"
+                placeholder="maximum amount"
+                onChange={this.formchange}
+                name="maxAmt"
+                addonAfter={this.state.currency || 'USD'}
+              />,
+            )}
+          </FormItem>
+          <FormItem
+            label="Minimum"
+            labelCol={{ span: 5 }}
+            wrapperCol={{ span: 12 }}
+          >
+            {getFieldDecorator('minimum', {
+              rules: [
+                { required: true, message: 'Please enter minimum limit!' },
+              ],
+            })(
+              <Input
+                type="number"
+                placeholder="minimum amount"
+                onChange={this.formchange}
+                name="minAmt"
+                addonAfter={this.state.currency || 'USD'}
+              />,
+            )}
+          </FormItem>
 
-        <FormItem wrapperCol={{ span: 12, offset: 5 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </FormItem>
-      </Form>
+          <FormItem
+            label="@ Price"
+            labelCol={{ span: 5 }}
+            wrapperCol={{ span: 12 }}
+          >
+            {getFieldDecorator('atPrice', {
+              rules: [{ required: true, message: 'Please Choose Your Price' }],
+            })(
+              <RadioGroup
+                onChange={this.onRadioChange}
+                value={this.state.atPrice}
+              >
+                <Radio value={1}>Float By Market Price</Radio>
+                <Radio value={2}>Fixed Price</Radio>
+              </RadioGroup>,
+            )}
+          </FormItem>
+          {this.state.atPrice === 2 && (
+            <FormItem
+              label="fixedPrice"
+              labelCol={{ span: 5 }}
+              wrapperCol={{ span: 12 }}
+            >
+              {getFieldDecorator('fixedPrice', {
+                rules: [
+                  { required: true, message: 'Please enter your fixed Price!' },
+                ],
+              })(
+                <Input
+                  type="number"
+                  placeholder="fixed Price amount"
+                  onChange={this.formchange}
+                  name="fixedPrice"
+                  addonAfter={this.state.currency || 'USD'}
+                />,
+              )}
+            </FormItem>
+          )}
+
+          <FormItem wrapperCol={{ span: 12, offset: 5 }}>
+            <Button
+              type="primary"
+              htmlType="submit"
+            >
+              Submit
+            </Button>
+          </FormItem>
+        </Form>
+      </div>
     );
   }
 }
@@ -229,9 +329,8 @@ class Addp2p extends React.Component {
   getFieldDecorator = this.props.form;
   contentListNoTitle = {
     buy: <BuyComponent form={this.props.form} />,
-    sell: <div>Hii</div>,
+    sell: <BuyComponent form={this.props.form} sell={true} />,
   };
-
   render() {
     return (
       <div className={s.root}>
