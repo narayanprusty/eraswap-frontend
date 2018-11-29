b95b59778179c6ddce6a658d666f8740b9b3930b
Utkarsh Mehta <utkarsh.mehta678@live.com>
2018-11-29 19:02:38 +0530

    UI and wallet api linking

--- src/routes/wallet/Wallet.js
+++ src/routes/wallet/Wallet.js
@@ -62,6 +62,7 @@ class WalletManager extends React.Component{
 
     componentDidMount(){
         this.getBalance();
+        this.getAddress();
     }
 
     onTabChange = (key) => {
@@ -70,11 +71,34 @@ class WalletManager extends React.Component{
     };
 
     send = () => {
-        console.log(this.state.recipient, this.state.amount);
+        var data = {
+            crypto : this.state.name,
+            receiver : this.state.recipient,
+            amount: this.state.amount
+        };
+        axios.post('/apis/wallet/send', data).then(res => {
+            console.log(res);
+        }).catch(error => {
+            console.log(error);
+        });;
     };
 
     getBalance = () => {
-        this.setState({balance: 1234.009});
+        axios.get('/apis/wallet/getBalance?crypto=' + this.state.name).then(res => {
+            console.log(res.data.balance);
+            this.setState({ balance: res.data.balance });
+        }).catch(error => {
+            console.log(error);
+        });
+    };
+
+    getAddress = () => {
+        axios.get('/apis/wallet/getAddress?crypto=' + this.state.name).then(res => {
+            console.log(res.data.address);
+            this.setState({ ourWallet: res.data.address });
+        }).catch(error => {
+            console.log(error);
+        });
     };
 
     downloadPrivateKey = () =>{
@@ -153,7 +177,7 @@ class WalletManager extends React.Component{
                             <Input.Search
                               style={{ maxWidth: '45.2%' }}
                               size="large"
-                              defaultValue={this.state.ourWallet}
+                              value={this.state.ourWallet}
                               enterButton={<Icon type="copy" />}
                               onSearch={value => {
                                 this.copyToClipboard(value);
