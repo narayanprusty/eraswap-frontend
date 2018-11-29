ad43a6a00fbf55d31414b04093bd3312c8372123
saikat <saikatchakrabortty2@gmail.com>
2018-11-30 00:51:41 +0530

    Chnage in case

--- src/routes/wallet/AllWallets.js
+++ src/routes/wallet/AllWallets.js
@@ -4,7 +4,7 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './AllWallets.css';
 import { Button, Slider, Row, Col, Card, Icon, Steps } from 'antd';
 import Link from '../../components/Link';
-import walletSvg from './Wallet.svg';
+import walletSvg from './wallet.svg';
 
 class AllWallets extends React.Component {
   static propTypes = {
@@ -24,41 +24,46 @@ class AllWallets extends React.Component {
     return (
       <div className={s.root}>
         <div className={s.container}>
-
-          <Card style={{ margin: '20px 0 '}}>
-            <Row type="flex" justify="space-around" style={{ margin: '20px 0 '}}>
-            <Col span={8}>
-            <div className={s.dashIcon}>
-                <Link to="/wallet/Btc">
-                {/* <Icon type="wallet" theme="twoTone" className={s.icoStyle} /> */}
-                <img src={walletSvg} className={s.icoStyle} />
-                <br />
-                <br />
-                <span className={s.icoBullet}>Bitcoin</span><br />
-                </Link>
-         </div>
-              </Col>
+          <Card style={{ margin: '20px 0 ' }}>
+            <Row
+              type="flex"
+              justify="space-around"
+              style={{ margin: '20px 0 ' }}
+            >
               <Col span={8}>
-              <div className={s.dashIcon}>
-                <Link to="/wallet/Eth">
-                {/* <Icon type="database" theme="twoTone"  className={s.icoStyle} /> */}
-                <img src={walletSvg} className={s.icoStyle} />
-                <br />
-                <br />
-                <span className={s.icoBullet}>Ethereum</span><br />
-
-                </Link>
+                <div className={s.dashIcon}>
+                  <Link to="/wallet/Btc">
+                    {/* <Icon type="wallet" theme="twoTone" className={s.icoStyle} /> */}
+                    <img src={walletSvg} className={s.icoStyle} />
+                    <br />
+                    <br />
+                    <span className={s.icoBullet}>Bitcoin</span>
+                    <br />
+                  </Link>
                 </div>
               </Col>
               <Col span={8}>
-              <div className={s.dashIcon}>
-                <Link to="/wallet/Est">
-                {/* <Icon type="swap" theme="twoTone" className={s.icoStyle} /> */}
-                <img src={walletSvg} className={s.icoStyle} />
-                <br />
-                <br />
-                <span className={s.icoBullet}>EST Token</span><br />
-                </Link>
+                <div className={s.dashIcon}>
+                  <Link to="/wallet/Eth">
+                    {/* <Icon type="database" theme="twoTone"  className={s.icoStyle} /> */}
+                    <img src={walletSvg} className={s.icoStyle} />
+                    <br />
+                    <br />
+                    <span className={s.icoBullet}>Ethereum</span>
+                    <br />
+                  </Link>
+                </div>
+              </Col>
+              <Col span={8}>
+                <div className={s.dashIcon}>
+                  <Link to="/wallet/Est">
+                    {/* <Icon type="swap" theme="twoTone" className={s.icoStyle} /> */}
+                    <img src={walletSvg} className={s.icoStyle} />
+                    <br />
+                    <br />
+                    <span className={s.icoBullet}>EST Token</span>
+                    <br />
+                  </Link>
                 </div>
               </Col>
             </Row>
