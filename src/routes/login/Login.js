696bcde0c06af48e72647c77e2db702e567d13ae
saikat <saikatchakrabortty2@gmail.com>
2018-12-18 19:27:08 +0530

    Social Logins Added

--- src/routes/login/Login.js
+++ src/routes/login/Login.js
@@ -12,9 +12,19 @@ import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Login.css';
 import axios from 'axios';
-import { Redirect } from 'react-router';
+import {Spin,Card} from 'antd';
 import queryString from 'stringquery';
-
+import { from } from 'zen-observable';
+const SOCIAL ={
+  GOOGLE:{
+      CLIENT_ID:'524726124380-u1nngf3k396jhtgrbmnqc6gchvsr6s3k.apps.googleusercontent.com',
+      REDIRECT_URI:'http://ec2-18-220-230-245.us-east-2.compute.amazonaws.com:3000/login'
+  },
+  FB:{
+      CLIENT_ID:"570289253420635",
+      REDIRECT_URI:'https://7d9a37c6.ngrok.io/login'  //FB needs HTTPS only
+  }
+}
 class Login extends React.Component {
   constructor(props) {
     super(props);
@@ -28,9 +38,72 @@ class Login extends React.Component {
     title: PropTypes.string.isRequired,
   };
 
+
+  fbLogin =(queries)=>{
+    const data = {
+     state:queries.state,
+     code:queries.code
+    };
+    axios
+      .post('/auth/login/fb', data)
+      .then(response => {
+        if (response.data) {
+          console.log(response.data);
+
+          for (const i in response.data) {
+            localStorage.setItem(i, JSON.stringify(response.data[i]));
+          }
+          window.location.href = '/';
+        }
+      }).catch(error=>{
+        this.setState({
+          loader:false
+        })
+        console.log(error);
+      })
+
+  }
+  googleLogin =(queries)=>{
+    const data = {
+      state:queries.state,
+      code:queries.code
+     };
+     axios
+       .post('/auth/login/google', data)
+       .then(response => {
+         if (response.data) {
+           console.log(response.data);
+
+           for (const i in response.data) {
+             localStorage.setItem(i, JSON.stringify(response.data[i]));
+           }
+           window.location.href = '/';
+         }
+       }).catch(error=>{
+        this.setState({
+          loader:false
+        })
+        console.log(error);
+       })
+
+  }
   componentDidMount = () => {
     const queries = queryString(location.search);
-    if (queries.how != 'force') {
+    debugger;
+    if(queries.code && queries.state=="fb"){
+      this.setState({
+        loader:true,
+        type: 'Facebook'
+      });
+      this.fbLogin(queries);
+    }else if(queries.code && queries.state=="google"){
+      this.setState({
+        loader:true,
+        type:'Google'
+      });
+      this.googleLogin(queries);
+    }
+    else if (queries.how != 'force') {
       axios
         .get('/apis/ping')
         .then(data => {
@@ -44,6 +117,7 @@ class Login extends React.Component {
     } else {
       console.log('just skipping');
     }
+
   };
 
   handleChanges = e => {
@@ -78,10 +152,17 @@ class Login extends React.Component {
   render() {
     return (
       <div className={s.root}>
+      {this.state.loader &&(
+        <Card style={{textAlign:"center"}}>
+          Please wait while we try to login you using {this.state.type}<br />
+          <Spin size='large' />
+        </Card>
+      )}
+      {!this.state.loader &&(
         <div className={s.container}>
           {/* <h1>{this.props.title}</h1> */}
           <div className={s.formGroup} style={{paddingTop:"3em"}}>
-            <a className={s.facebook} href="/login/facebook">
+            <a className={s.facebook} href={`https://www.facebook.com/v3.2/dialog/oauth?client_id=${SOCIAL.FB.CLIENT_ID}&redirect_uri=${SOCIAL.FB.REDIRECT_URI}&state=fb&scope=email`}>
               <svg
                 className={s.icon}
                 width="30"
@@ -95,7 +176,7 @@ class Login extends React.Component {
             </a>
           </div>
           <div className={s.formGroup}>
-            <a className={s.google} href="/login/google">
+            <a className={s.google} href={`https://accounts.google.com/o/oauth2/auth?client_id=${SOCIAL.GOOGLE.CLIENT_ID}&redirect_uri=${SOCIAL.GOOGLE.REDIRECT_URI}&scope=email&response_type=code&state=google`}>
               <svg
                 className={s.icon}
                 width="30"
@@ -119,30 +200,7 @@ class Login extends React.Component {
               <span>Log in with Google</span>
             </a>
           </div>
-          <div className={s.formGroup}>
-            <a className={s.twitter} href="/login/twitter">
-              <svg
-                className={s.icon}
-                width="30"
-                height="30"
-                viewBox="0 0 30 30"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d={
-                    'M30 6.708c-1.105.49-2.756 1.143-4 1.292 1.273-.762 2.54-2.56 ' +
-                    '3-4-.97.577-2.087 1.355-3.227 1.773L25 5c-1.12-1.197-2.23-2-4-2-3.398 0-6 ' +
-                    '2.602-6 6 0 .4.047.7.11.956L15 10C9 10 5.034 8.724 2 5c-.53.908-1 1.872-1 ' +
-                    '3 0 2.136 1.348 3.894 3 5-1.01-.033-2.17-.542-3-1 0 2.98 4.186 6.432 7 7-1 ' +
-                    '1-4.623.074-5 0 .784 2.447 3.31 3.95 6 4-2.105 1.648-4.647 2.51-7.53 2.51-.5 ' +
-                    '0-.988-.03-1.47-.084C2.723 27.17 6.523 28 10 28c11.322 0 17-8.867 17-17 ' +
-                    '0-.268.008-.736 0-1 1.2-.868 2.172-2.058 3-3.292z'
-                  }
-                />
-              </svg>
-              <span>Log in with Twitter</span>
-            </a>
-          </div>
+
           <strong className={s.lineThrough}>OR</strong>
           <p className={s.lead} style={{textAlign:"center"}}>
             Log in with your username and password.
@@ -180,6 +238,7 @@ class Login extends React.Component {
             </div>
           </form>
         </div>
+      )}
       </div>
     );
   }
