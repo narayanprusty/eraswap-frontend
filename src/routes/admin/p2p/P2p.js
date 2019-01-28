54716b1c00b15f54ba7b30a8e9a04bc4fbfcd520
saikat <saikatchakrabortty2@gmail.com>
2019-01-28 17:54:19 +0530

    notifications added

--- src/routes/admin/p2p/P2p.js
+++ src/routes/admin/p2p/P2p.js
@@ -2,7 +2,7 @@ import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import axios from 'axios';
 import Link from '../../../components/Link';
-import { Card, Table,Tag,Button } from 'antd';
+import { Card, Table, Tag, Button, notification } from 'antd';
 import s from './P2p.css';
 
 class P2p extends React.Component {
@@ -18,74 +18,89 @@ class P2p extends React.Component {
     this.setState({
       pagination: pager,
     });
-    this.fetch({},{
-      results: pagination.pageSize,
-      page: pagination.current,
-      sortField: sorter.field,
-      sortOrder: sorter.order,
-      ...filters,
-    });
-  }
+    this.fetch(
+      {},
+      {
+        results: pagination.pageSize,
+        page: pagination.current,
+        sortField: sorter.field,
+        sortOrder: sorter.order,
+        ...filters,
+      },
+    );
+  };
 
-  backToSeller = (record)=>{
-    const data ={
-      matchId:record.uniqueIdentifier,
-      listingId:record.listingId,
-      owner:record.ownerUser,
-      requester:record.requester,
-      amount:record.amount,
-      fee:record.fee,
-      cryptoCurrency:record.cryptoCurrency
-    }
-    axios.post('/admins/apis/p2p/dispute/send_back_to_seller',data).then(data=>{
+  backToSeller = record => {
+    notification.open({
+      message: 'Raising request please wait!',
+    });
+    const data = {
+      matchId: record.uniqueIdentifier,
+      listingId: record.listingId,
+      owner: record.ownerUser,
+      requester: record.requester,
+      amount: record.amount,
+      fee: record.fee,
+      cryptoCurrency: record.cryptoCurrency,
+    };
+    axios
+      .post('/admins/apis/p2p/dispute/send_back_to_seller', data)
+      .then(data => {
+        notification.open({
+          message: 'Refunded back to seller!',
+        });
+        return data;
+      });
+  };
+
+  sendToBuyer = record => {
+    notification.open({
+      message: 'Raising request please wait!',
+    });
+    const data = {
+      matchId: record.uniqueIdentifier,
+      listingId: record.listingId,
+      owner: record.ownerUser,
+      requester: record.requester,
+      amount: record.amount,
+      cryptoCurrency: record.cryptoCurrency,
+    };
+    axios.post('/admins/apis/p2p/dispute/send_to_buyer', data).then(data => {
+      notification.open({
+        message: 'Refunded back to buyer!',
+      });
       return data;
     });
-  }
+  };
 
-  sendToBuyer = (record)=>{
-    const data ={
-      matchId:record.uniqueIdentifier,
-      listingId:record.listingId,
-      owner:record.ownerUser,
-      requester:record.requester,
-      amount:record.amount,
-      cryptoCurrency:record.cryptoCurrency
-    }
-    axios.post('/admins/apis/p2p/dispute/send_to_buyer',data).then(data=>{
-      return data;
-    });
-  }
-
-  fetch = (query,params = {}) => {
+  fetch = (query, params = {}) => {
     console.log('params:', params);
     this.setState({ loading: true });
-    axios.get('/admins/apis/p2p/list_count').then(countData=>{
-      if(countData.data)
-    axios({
-      url: '/admins/apis/p2p/list',
-      params:{
-        results:10,
-        ...params,
-        query:query
-      },
-      method: 'get',
-      type: 'json',
-    }).then(async(data) => {
-      if(data.data){
-      const pagination = { ...this.state.pagination };
-      pagination.total = countData.data.totalCount.message
+    axios.get('/admins/apis/p2p/list_count').then(countData => {
+      if (countData.data)
+        axios({
+          url: '/admins/apis/p2p/list',
+          params: {
+            results: 10,
+            ...params,
+            query: query,
+          },
+          method: 'get',
+          type: 'json',
+        }).then(async data => {
+          if (data.data) {
+            const pagination = { ...this.state.pagination };
+            pagination.total = countData.data.totalCount.message;
 
-      this.setState({
-        loading: false,
-        data: data.data,
-        pagination,
-      });
-    }
+            this.setState({
+              loading: false,
+              data: data.data,
+              pagination,
+            });
+          }
+        });
     });
-
-  });
-  }
-
+  };
 
   componentDidMount() {
     if (
@@ -98,94 +113,183 @@ class P2p extends React.Component {
       localStorage.clear();
       location.href = '/login?how=force';
     }
-   this.fetch();
+    this.fetch();
   }
 
-  columns = [{
-    title:'Listing Id',
-    dataIndex:'listingId'
-  },
-  {
-    title:'Amount',
-    dataIndex:'amount'
-  },
-  {
-    title:'Listing Owner',
-    dataIndex:'ownerUser',
-    render:(fieldval)=>{
-      if(fieldval){
-        return <div>{fieldval.username} <br /> {fieldval.email}</div>
-        }else{
-          return 'User does not exist.'
+  columns = [
+    {
+      title: 'Listing Id',
+      dataIndex: 'listingId',
+    },
+    {
+      title: 'Amount',
+      dataIndex: 'amount',
+    },
+    {
+      title: 'Listing Owner',
+      dataIndex: 'ownerUser',
+      render: fieldval => {
+        if (fieldval) {
+          return (
+            <div>
+              {fieldval.username} <br /> {fieldval.email}
+            </div>
+          );
+        } else {
+          return 'User does not exist.';
         }
-    }
-  },
-  {
-    title:'Requester',
-    dataIndex:'requester',
-    render:(fieldval)=>{
-      if(fieldval){
-      return <div>{fieldval.username} <br /> {fieldval.email}</div>
-      }else{
-        return 'User does not exist.'
-      }
-
-    }
-  },
-  {
-    title:'Fee',
-    dataIndex:'fee',
-    render:(fieldval,record)=>{
-      if(fieldval){
-      return <div>{fieldval.toFixed(3)} / {record.feeCoin ? record.feeCoin :'-'}</div>
-      }else{
-        return <Tag color="volcano">Not Paid [Attention]</Tag>
-      }
-
-    }
-  },
-  ,{
-    title:'Buyer Paid',
-    dataIndex:"iPaidVal",
-    align:'center',
-    render: (fieldval,record)=>{
-      if(fieldval == true){
-      return <div><Tag color="green">Yes</Tag>&nbsp;<Button size='small' type='danger' onClick={this.backToSeller.bind(this,record)}  disabled={record.finished || record.sendToBuyer || record.backToSeller ? true :false} ghost> Send to seller</Button></div>
-    }else{
-      return <div><Tag color="volcano">No</Tag>&nbsp;<Button size='small' type='danger' onClick={this.backToSeller.bind(this,record)}  disabled={record.finished || record.sendToBuyer || record.backToSeller ? true :false} ghost> Send to seller</Button></div>
-    }
-  }
-  },{
-    title:'Seller Released',
-    dataIndex:"finished",
-    align:'center',
-    render: (fieldval,record)=>{
-      if(fieldval == true){
-      return <div> <Tag color="green">Yes</Tag>&nbsp;<Button size='small' type='danger' onClick={this.sendToBuyer.bind(this,record)}  disabled={record.finished || record.sendToBuyer || record.backToSeller || !record.iPaidVal ? true :false} ghost> Send to Buyer</Button> </div>
-    }else{
-      return <div> <Tag color="volcano">No</Tag>&nbsp;<Button size='small' type='danger' onClick={this.sendToBuyer.bind(this,record)}  disabled={record.finished || record.sendToBuyer || record.backToSeller || !record.iPaidVal ? true :false} ghost> Send to Buyer</Button> </div>
-    }
-  }
-  }
-]
+      },
+    },
+    {
+      title: 'Requester',
+      dataIndex: 'requester',
+      render: fieldval => {
+        if (fieldval) {
+          return (
+            <div>
+              {fieldval.username} <br /> {fieldval.email}
+            </div>
+          );
+        } else {
+          return 'User does not exist.';
+        }
+      },
+    },
+    {
+      title: 'Fee',
+      dataIndex: 'fee',
+      render: (fieldval, record) => {
+        if (fieldval) {
+          return (
+            <div>
+              {fieldval.toFixed(3)} / {record.feeCoin ? record.feeCoin : '-'}
+            </div>
+          );
+        } else {
+          return <Tag color="volcano">Not Paid [Attention]</Tag>;
+        }
+      },
+    },
+    ,
+    {
+      title: 'Buyer Paid',
+      dataIndex: 'iPaidVal',
+      align: 'center',
+      render: (fieldval, record) => {
+        if (fieldval == true) {
+          return (
+            <div>
+              <Tag color="green">Yes</Tag>&nbsp;<Button
+                size="small"
+                type="danger"
+                onClick={this.backToSeller.bind(this, record)}
+                disabled={
+                  record.finished || record.sendToBuyer || record.backToSeller
+                    ? true
+                    : false
+                }
+                ghost
+              >
+                {' '}
+                Send to seller
+              </Button>
+            </div>
+          );
+        } else {
+          return (
+            <div>
+              <Tag color="volcano">No</Tag>&nbsp;<Button
+                size="small"
+                type="danger"
+                onClick={this.backToSeller.bind(this, record)}
+                disabled={
+                  record.finished || record.sendToBuyer || record.backToSeller
+                    ? true
+                    : false
+                }
+                ghost
+              >
+                {' '}
+                Send to seller
+              </Button>
+            </div>
+          );
+        }
+      },
+    },
+    {
+      title: 'Seller Released',
+      dataIndex: 'finished',
+      align: 'center',
+      render: (fieldval, record) => {
+        if (fieldval == true) {
+          return (
+            <div>
+              {' '}
+              <Tag color="green">Yes</Tag>&nbsp;<Button
+                size="small"
+                type="danger"
+                onClick={this.sendToBuyer.bind(this, record)}
+                disabled={
+                  record.finished ||
+                  record.sendToBuyer ||
+                  record.backToSeller ||
+                  !record.iPaidVal
+                    ? true
+                    : false
+                }
+                ghost
+              >
+                {' '}
+                Send to Buyer
+              </Button>{' '}
+            </div>
+          );
+        } else {
+          return (
+            <div>
+              {' '}
+              <Tag color="volcano">No</Tag>&nbsp;<Button
+                size="small"
+                type="danger"
+                onClick={this.sendToBuyer.bind(this, record)}
+                disabled={
+                  record.finished ||
+                  record.sendToBuyer ||
+                  record.backToSeller ||
+                  !record.iPaidVal
+                    ? true
+                    : false
+                }
+                ghost
+              >
+                {' '}
+                Send to Buyer
+              </Button>{' '}
+            </div>
+          );
+        }
+      },
+    },
+  ];
 
   render() {
     return (
       <div className={s.root}>
-      <Card>
-      <Table
-        style={{overflowX: 'scroll'}}
-      columns={this.columns}
-      rowKey={record => record._id}
-      dataSource={this.state.data}
-      pagination={this.state.pagination}
-      loading={this.state.loading}
-      onChange={this.handleTableChange}
-    />
-    </Card>
-    </div>
-    )}
+        <Card>
+          <Table
+            style={{ overflowX: 'scroll' }}
+            columns={this.columns}
+            rowKey={record => record._id}
+            dataSource={this.state.data}
+            pagination={this.state.pagination}
+            loading={this.state.loading}
+            onChange={this.handleTableChange}
+          />
+        </Card>
+      </div>
+    );
+  }
 }
-
 
 export default withStyles(s)(P2p);
